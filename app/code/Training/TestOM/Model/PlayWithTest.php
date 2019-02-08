215df9395186e63ae6a11117aeaf952d10b04a00
SerMall <serhima@gmail.com>
2019-02-09 01:39:28 +0200

    HW task 1.6

---
+++ app/code/Training/TestOM/Model/PlayWithTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Training\TestOM\Model;
+
+class PlayWithTest
+{
+    private $testObject;
+    private $testObjectFactory;
+    private $manager;
+
+    public function __construct(
+        Test $testObject,
+        TestFactory $testObjectFactory,
+        ManagerCustomImplementation $manager
+    ) {
+        $this->testObject = $testObject;
+        $this->testObjectFactory = $testObjectFactory;
+        $this->manager = $manager;
+    }
+
+    public function run()
+    {
+        $this->testObject->log();
+
+        $customArrayList = ['item1' => 'aaaaa', 'item2' => 'bbbbb'];
+        $newTestObject = $this->testObjectFactory->create([
+            'arrayList' => $customArrayList,
+            'manager' => $this->manager
+        ]);
+        $newTestObject->log();
+    }
+
+}
