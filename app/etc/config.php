3e5b17f2f25c7b341306d9131a8d0d95d26c9037
SerMall <serhima@gmail.com>
2019-02-10 00:16:06 +0200

    HW task 1.7

--- app/etc/config.php
+++ app/etc/config.php
@@ -193,6 +193,7 @@ return [
         'Training_Dependency2' => 1,
         'Training_Dependency' => 1,
         'Training_TestOM' => 1,
+        'Training_Test' => 1,
         'Training_AdditionalTestOM' => 1,
         'Vertex_Tax' => 1
     ]
