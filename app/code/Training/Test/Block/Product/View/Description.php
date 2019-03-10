84426f697bbab27c75b9f8e842cc43b4ddc6bfc4
SerMall <serhima@gmail.com>
2019-03-10 17:51:57 +0200

    HW task 3.7 three template

--- app/code/Training/Test/Block/Product/View/Description.php
+++ app/code/Training/Test/Block/Product/View/Description.php
@@ -7,7 +7,7 @@ class Description
     public function beforeToHtml(
         \Magento\Catalog\Block\Product\View\Description $subject
     ) {
-        $subject->getProduct()->setDescription('Test Description 2222');
-        return $subject;
+//        $subject->getProduct()->setDescription('Test Description 333');
+        $subject->setTemplate('Training_Test::description.phtml');
     }
 }
