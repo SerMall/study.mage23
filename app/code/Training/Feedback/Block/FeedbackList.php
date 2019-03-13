908d9466eb86980ef85af1c4e2d50d7a71f04d9e
SerMall <serhima@gmail.com>
2019-03-13 22:45:06 +0200

    HW task 5.2

--- app/code/Training/Feedback/Block/FeedbackList.php
+++ app/code/Training/Feedback/Block/FeedbackList.php
@@ -9,17 +9,20 @@ class FeedbackList extends \Magento\Framework\View\Element\Template
     const PAGE_SIZE = 5;
 
     private $collectionFactory;
+    private $feedbackResource;
     private $collection;
     private $timezone;
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Training\Feedback\Model\ResourceModel\Feedback\CollectionFactory $collectionFactory,
+        \Training\Feedback\Model\ResourceModel\Feedback $feedbackResource,
         \Magento\Framework\Stdlib\DateTime\Timezone $timezone,
         array $data = array()
     ) {
         parent::__construct($context, $data);
         $this->collectionFactory = $collectionFactory;
+        $this->feedbackResource = $feedbackResource;
         $this->timezone = $timezone;
     }
 
@@ -59,8 +62,18 @@ class FeedbackList extends \Magento\Framework\View\Element\Template
         return $this->getUrl('training_feedback/index/form');
     }
 
-     public function getFeedbackDate($feedback)
+    public function getFeedbackDate($feedback)
     {
         return $this->timezone->formatDateTime($feedback->getCreatingTime());
+    }
+
+    public function getAllFeedbackNumber()
+    {
+        return $this->feedbackResource->getAllFeedbackNumber();
+    }
+
+    public function getActiveFeedbackNumber()
+    {
+        return $this->feedbackResource->getActiveFeedbackNumber();
     }
 }
